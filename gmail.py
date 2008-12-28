d1dfa4bc72a5bc7c6a143b5372aa0a45571cade9
Giuliani Vito Ivan <giuliani.v@gmail.com>
2008-12-29 00:40:09 +0100

    Added GMail support

---
+++ gmail.py
@@ -0,0 +1,40 @@
+import time
+import threading
+import libgmail
+
+from settings import GMAIL_USER, GMAIL_PASS, GMAIL_CHECK_INTERVAL
+
+class GmailOsd(threading.Thread):
+    def __init__(self, osd, *args, **kwargs):
+        self.osd = osd
+        self.connected = False
+
+        super(GmailOsd, self).__init__(*args, **kwargs)
+
+    def run(self):
+        self.osd.display("Connecting to GMail...")
+
+        self.ga = libgmail.GmailAccount(GMAIL_USER, GMAIL_PASS)
+
+        try:
+            self.ga.login()
+        except libgmail.GmailLoginFailure:
+            self.osd.display("GMail login failure!")
+
+        while True:
+            self.gmail_check()
+            time.sleep(GMAIL_CHECK_INTERVAL * 60)
+
+    def gmail_check(self):
+        unread = self.ga.getUnreadMsgCount()
+
+        self.osd.set_colour("green")
+        if unread == 0:
+            self.osd.display("No unread emails!")
+        else:
+            if unread > 30:
+                self.osd.set_colour("red")
+            elif unread > 15:
+                self.osd.set_colour("yellow")
+
+            self.osd.display("You got %d unread emails" % self.ga.getUnreadMsgCount())
