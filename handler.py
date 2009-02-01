fc91c56802dc1f0624dfb15182e94435d19f323e
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-02-01 19:23:25 +0100

    Activate widgets based on preferences specified in settings file

--- handler.py
+++ handler.py
@@ -7,12 +7,14 @@ import widget
 import gmail
 
 def handle():
-    osd_gmail = gmail.GMailWidget()
-    osd_batt = widget.BatteryWidget()
-    osd_temp = widget.TemperatureWidget()
-    osd_cpu = widget.CPUWidget()
+    w = {
+        'gmail':    gmail.GMailWidget,
+        'batt':     widget.BatteryWidget,
+        'temp':     widget.TemperatureWidget,
+        'cpu':      widget.CPUWidget,
+    }
 
-    widgets = [ osd_gmail, osd_batt, osd_temp, osd_cpu ]
+    widgets = [w[widget_name]() for widget_name in settings.widgets if widget_name in w.keys()]
 
     while True:
         [osd.display() for osd in widgets]
